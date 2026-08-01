2e82cda31a38f7b9bf0f5b672ab64ef2229b5bb4
saurabh4524 <saurabh.kumar@appsndevices.in>
2026-08-01 12:49:26 +0530

    redus connection

--- tenantiq-backend/src/config/redis.js
+++ tenantiq-backend/src/config/redis.js
@@ -1,16 +1,18 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-})
+const redis = process.env.REDIS_URL
+    ? new Redis(process.env.REDIS_URL)
+    : new Redis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+    });
 
-redis.on('connect', ()=>{
+redis.on('connect', () => {
     console.log('Redis connected');
 });
 
-redis.on('error',(err)=>{
-    console.error('Redis connection error', err)
-})
+redis.on('error', (err) => {
+    console.error('Redis connection error', err);
+});
 
 module.exports = redis;
